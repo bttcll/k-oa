7d2628b91415e27c02239bc6f370237047553a59
Marco <mrcbttc@gmail.com>
2021-01-23 14:49:32 +0100

    Prova funzione alternativa in gasdev

--- src/store/index.js
+++ src/store/index.js
@@ -87,8 +87,7 @@ export default new Vuex.Store({
 
         //extra per la dashboard
         builded: 0,
-        kDone: 0,
-        seed: 0
+        kDone: 0
     },
     mutations: {
         saveAll(state) {
@@ -104,7 +103,7 @@ export default new Vuex.Store({
                 type: "text/plain;charset=utf-8"
             });
             const d = new Date();
-            FileSaver.saveAs(blob, "session_" + d.getDate() + "_" + d.getMonth() + "_" + d.getFullYear()+".bin");
+            FileSaver.saveAs(blob, "session_" + d.getDate() + "_" + d.getMonth() + "_" + d.getFullYear() + ".bin");
 
         },
         loadAll(state, file) {
@@ -137,8 +136,6 @@ export default new Vuex.Store({
             state.nRealGrades = data.nRealGrades;
             state.builded = data.builded;
             state.kDone = data.kDone;
-            state.seed = data.seed;
-
         },
         resetAll(state) {
             localStorage.clear();
@@ -165,7 +162,6 @@ export default new Vuex.Store({
             state.nRealGrades = 0;
             state.builded = 0;
             state.kDone = 0;
-            state.seed = 0;
         },
         // state passa l'intero stato, c sono le variabili passate dai components
         GeneraMatriceAdiacenza: (state, c) => GeneraMatriceAdiacenza(state, c.NumSt, c.Voti, c.file, c.alfa, c.min, c.max),
@@ -722,22 +718,23 @@ function Gauss(state, lseed, numGen, mediaInput, varianzaInput, votoInf, votoSup
     let fp = "";
     let p, j, m;
     let voti = [];
-    state.lseed = lseed;
+    const l = { seed: 0 };
+    l.seed = lseed;
 
     //generaRand(voti,numGen,votoInf,votoSup);
     //system("pause");
     for (let i = 0; i < numGen; i++) {
-        p = gasdev(state);
+        p = gasdev(l);
         voti[i] = (varianzaInput * p + mediaInput);
         // arrotondamento a 0.5
         j = parseInt(voti[i]); // per difetto
         m = j + 1;
-        if (voti[i] <= (j) + 0.5)
-            voti[i] = (j);
+        if (voti[i] <= j + 0.5)
+            voti[i] = j;
         else
-            voti[i] = (m);
+            voti[i] = m;
         while (voti[i] > (votoSup) || voti[i] < votoInf) {
-            voti[i] = (varianzaInput * gasdev(state) + mediaInput);
+            voti[i] = varianzaInput * gasdev(l) + mediaInput;
             j = parseInt(voti[i]); // per difetto
             m = j + 1;
             if (voti[i] <= j + 0.5)
@@ -764,23 +761,21 @@ function Gauss(state, lseed, numGen, mediaInput, varianzaInput, votoInf, votoSup
 
     return;
 }
-function gasdev(state) {
+function gasdev(l) {
 
     let iset = 0;
     let gset;
     let fac, rsq, v1, v2;
-    let i = 0;
 
-    if (state.seed < 0) iset = 0;
+    if (l.seed < 0) iset = 0;
     if (iset == 0) {
         do {
-            v1 = 2.0 * ran1(state) - 1.0;
-            v2 = 2.0 * ran1(state) - 1.0;
-            console.log("v1: ", v1);
-            console.log("v2: ", v2);
+            l.seed = Math.random() * (Math.random() < 0.5 ? -1 : 1);
+            v1 = 2.0 * l.seed - 1.0;
+            l.seed = Math.random() * (Math.random() < 0.5 ? -1 : 1);
+            v2 = 2.0 * l.seed - 1.0;
             rsq = v1 * v1 + v2 * v2;
-            i++
-        } while (i < 10);
+        } while (rsq >= 1.0 || rsq == 0.0);
         fac = Math.sqrt(-2.0 * Math.log(rsq) / rsq);
         gset = v1 * fac;
         iset = 1;
@@ -789,45 +784,6 @@ function gasdev(state) {
         iset = 0;
         return gset;
     }
-}
-
-function ran1(state) {
-    // costanti
-    const IA = 16807;
-    const IM = 2147483647;
-    const AM = (1.0 / IM);
-    const IQ = 127773;
-    const IR = 2836;
-    const NTAB = 32;
-    const NDIV = (1 + (IM - 1) / NTAB);
-    const EPS = 1.2e-7;
-    const RNMX = (1.0 - EPS);
-
-    let j;
-    let k;
-    let iy = 0;
-    let iv = [NTAB];
-    let temp;
-
-    if (state.seed <= 0 || !iy) {
-        if (-(state.seed) < 1) state.seed = 1;
-        else state.seed = -(state.seed);
-        for (j = NTAB + 7; j >= 0; j--) {
-            k = (state.seed) / IQ;
-            state.seed = IA * (state.seed - k * IQ) - IR * k;
-            if (state.seed < 0) state.seed += IM;
-            if (j < NTAB) iv[j] = state.seed;
-        }
-        iy = iv[0];
-    }
-    k = (state.seed) / IQ;
-    state.seed = IA * (state.seed - k * IQ) - IR * k;
-    if (state.seed < 0) state.seed += IM;
-    j = parseInt(iy / NDIV);
-    iy = iv[j];
-    iv[j] = state.seed;
-    if ((temp = AM * iy) > RNMX) return RNMX;
-    else return temp;
 }
 
 function InitializeStudentModelByTeacher(state, delta) {
